d2075be528a53ebd3ae8b2e7cd203bee8f590e87
Aarya <24khobaragadea@rbunagpur.in>
2026-05-21 23:03:01 +0530

    Added Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,17 @@
+# TUPLES ARE IMMUTABLE YOU CANT CHANGE IT AFTER CREATION 
+
+t=(10,20,30,40)
+point=(12.2,2.3)
+single=(34,)
+
+print(t[1])
+print(len(t))
+
+# SWAP TWO NUMBERS
+
+a ,b=5,10
+a,b=b,a
+print(a,b)
+
+a ,b ,c=t
+print(a ,b ,c)
